c00ddc6dae3e7444318b419331ad2d0aa1773e73
Jakaria030 <gulamjakaria99@gmail.com>
2026-01-15 16:46:13 +0600

    Not found page design complete

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,14 @@
-
+import notFound from "../assets/notFound.jpg";
+import CTA from "../components/CTA.jsx";
 
 const NotFound = () => {
-
     return (
-        <div>
-            NotFound
+        <div className="min-h-screen flex items-center justify-center">
+            <section className="rounded-lg shadow-md text-center space-y-4 pb-8 mx-8 md:mx-16">
+                <img className="w-3xl h-auto" src={notFound} alt="404 Image" />
+                <p className="text-gray-500">Page not found!</p>
+                <CTA>Home</CTA>
+            </section>
         </div>
     )
 };
