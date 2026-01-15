124eb0d735fdcae8b5cb1b8c5a4f07b3d58eb464
Jakaria030 <gulamjakaria99@gmail.com>
2026-01-15 16:57:26 +0600

    Changed not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,4 +1,4 @@
-import notFound from "../assets/notFound.jpg";
+import notFound from "../assets/not-found.jpg";
 import CTA from "../components/CTA.jsx";
 
 const NotFound = () => {
@@ -6,8 +6,8 @@ const NotFound = () => {
         <div className="min-h-screen flex items-center justify-center">
             <section className="rounded-lg shadow-md text-center space-y-4 pb-8 mx-8 md:mx-16">
                 <img className="w-3xl h-auto" src={notFound} alt="404 Image" />
-                <p className="text-gray-500">Page not found!</p>
-                <CTA>Home</CTA>
+                <p className="text-gray-500">Page Not Found!</p>
+                <CTA>HOME</CTA>
             </section>
         </div>
     )
