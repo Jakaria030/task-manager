b67c8b82394a61e34d82c9f2877d3dc1a4360f12
Jakaria030 <gulamjakaria99@gmail.com>
2026-01-15 16:11:11 +0600

    Add footer design

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
 
     return (
-        <div>
+        <div className="flex flex-col min-h-screen">
             <Navbar />
-            <Outlet />
-            Footer
+            <main className="flex-grow">
+                <Outlet />
+            </main>
+            <Footer />
         </div>
     )
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+const Footer = () => {
+    return (
+        <footer className="bg-white border-t border-gray-100">
+            <div className="max-w-7xl mx-auto px-4 md:px-8">
+                <p className="text-center text-gray-800 py-4">&copy; {new Date().getFullYear()} - All rights reserved By - Task Manager.</p>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
