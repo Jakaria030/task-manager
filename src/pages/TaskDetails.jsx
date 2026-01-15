d339cf386edefd901bb9a4b3b7b92e2725beccc5
Jakaria030 <gulamjakaria99@gmail.com>
2026-01-15 16:18:26 +0600

    All pages created and routes add

---
+++ src/pages/TaskDetails.jsx
@@ -0,0 +1,11 @@
+
+const TaskDetails = () => {
+
+    return (
+        <div>
+            TaskDetails;
+        </div>
+    )
+};
+
+export default TaskDetails;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import App from "../App.jsx";
 import Home from "../pages/Home.jsx";
+import TaskList from "../pages/TaskList.jsx";
+import TaskDetails from "../pages/TaskDetails.jsx";
+import NotFound from "../pages/NotFound.jsx";
 
 const routes = createBrowserRouter([
     {
@@ -12,10 +15,18 @@ const routes = createBrowserRouter([
                 Component: Home,
             },
             {
-                path: "about",
-                element: <h1>HEllo</h1>
+                path: "task-list",
+                Component: TaskList,
+            },
+            {
+                path: "task-details",
+                Component: TaskDetails
             }
         ]
+    },
+    {
+        path: "*",
+        Component: NotFound,
     }
 ]);
 
