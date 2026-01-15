0a39181562d728984fcd5e02d79fae8d41e6b6a3
Jakaria030 <gulamjakaria99@gmail.com>
2026-01-15 17:28:03 +0600

    Footer text design changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     return (
         <footer className="bg-white border-t border-gray-100">
             <div className="max-w-7xl mx-auto px-4 md:px-8">
-                <p className="text-center text-gray-800 py-4">&copy; {new Date().getFullYear()} - All rights reserved By - Task Manager.</p>
+                <p className="text-center text-sm text-gray-500 py-4">&copy; {new Date().getFullYear()} - All rights reserved By - Task Manager.</p>
             </div>
         </footer>
     )
